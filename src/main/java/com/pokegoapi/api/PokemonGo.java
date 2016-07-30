9d34776439807bff52a71ed3a7497889e477947b
alberto__segura <alberto.segura.delgado@gmail.com>
2016-07-30 11:45:15 +0200

    fix api limit problems

--- src/main/java/com/pokegoapi/api/PokemonGo.java
+++ src/main/java/com/pokegoapi/api/PokemonGo.java
@@ -68,20 +68,25 @@ public class PokemonGo {
 			this.credentialProvider = credentialProvider;
 		}
 
-		playerProfile = null;
+		try{
+			playerProfile = null;
 
-		// send profile request to get the ball rolling
-		requestHandler = new RequestHandler(this, client);
-		Thread.sleep(300);
-		playerProfile = new PlayerProfile(this);
-		Thread.sleep(300);
-		inventories = new Inventories(this);
-		Thread.sleep(300);
+			// send profile request to get the ball rolling
+			requestHandler = new RequestHandler(this, client);
+			Thread.sleep(300);
+			playerProfile = new PlayerProfile(this);
+			Thread.sleep(300);
+			inventories = new Inventories(this);
+			Thread.sleep(300);
 
-		playerProfile.updateProfile();
-		Thread.sleep(300);
-		inventories.updateInventories();
-		Thread.sleep(300);
+			playerProfile.updateProfile();
+			Thread.sleep(300);
+			inventories.updateInventories();
+			Thread.sleep(300);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 
 		// should have proper end point now.
 		map = new Map(this);
